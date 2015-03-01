65432c100df0d2b04237d6ecffa7561c98b342bf
Art Mills <art@hamcois.com>
2015-03-01 05:18:56 +0000

    Backing out limit on show

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -26,7 +26,7 @@ class BusinessesController < ApplicationController
 	end
 
 	def show
-		@customers = @business.customers.includes(:phones).limit(100)
+		@customers = @business.customers.includes(:phones)
 	end
 
 	def edit
